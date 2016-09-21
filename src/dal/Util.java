d5f6212bf841df105d609bfeccacd6f834077304
mehklubba <paulsson.kalle@gmail.com>
2016-09-21 13:50:43 +0200

    lade till util

---
+++ src/dal/Util.java
@@ -0,0 +1,42 @@
+package dal;
+
+public class Util {
+
+	public static String getStudent() {
+		return "select * from student where spnr = ?";
+	}
+
+	// allt från en viss kurs
+
+	public static String getCourseInfo() {
+		return "select * from course where ccode = ?";
+	}
+
+	// Visa resultat för angiven kurs (alla studenter som tagit kursen och deras
+	// betyg)
+
+	public static String courseResult() {
+		return "select spnr, grade from studied where ccode = ?";
+	}
+
+	// studenter som inte är klara med en viss kurs - fråga erre
+	public static String notFinished() {
+		return "select spnr, sname from studying where ccode = ?";
+	}
+
+	// procent studenter som fått a på en särskild kurs
+	public static String AcedIt() {
+		return "select count (grade)*100 /(select count (*) from studied where ccode =?) from studied group by ccode, grade having ccode = ? and grade='a'";
+	}
+
+	// resultat för angiven student på en viss kurs
+	public static String studentResult() {
+		return "select grade from studied where ccode = ? and spnr = ?";
+	}
+
+	// högst genomströmmning
+	public static String mostThrough() {
+		return "select ccode, count(grade) from studied group by ccode,grade having grade !='u'";
+	}
+
+}
