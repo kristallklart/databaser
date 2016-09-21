dfee65ab6052e9772905792fcb0da7f5eef4fa8c
Josefin <Josefin@172.16.254.128>
2016-09-21 13:07:04 +0100

    hghghghg

--- src/dal/LoginData.java
+++ src/dal/LoginData.java
@@ -5,7 +5,7 @@ public class LoginData {
 	private static String url = "jdbc:sqlserver://localhost:1433;DatabaseName=DB";
 	private static String user = "test";
 	private static String pw = "test";
-
+	private static String test = "test";
 	public static String getUrl() {
 		return url;
 	}
