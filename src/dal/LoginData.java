db975dea1ffc96e8add276e99dd210f87d5c5762
mehklubba <paulsson.kalle@gmail.com>
2016-09-21 11:52:45 +0200

    test

--- src/dal/LoginData.java
+++ src/dal/LoginData.java
@@ -5,6 +5,7 @@ public class LoginData {
 	private String url = "jdbc:sqlserver://localhost:1433" + "Tenta";
 	private String user = "test";
 	private String pw = "test";
+	private String test = "teset";
 
 	public String getUrl() {
 		return url;
