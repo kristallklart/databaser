a072b88efdfe8d849a9f6eda6fb76ebfb417affd
mehklubba <paulsson.kalle@gmail.com>
2016-09-21 11:24:52 +0200

    ny LoginData

---
+++ src/dal/LoginData.java
@@ -0,0 +1,21 @@
+package dal;
+
+public class LoginData {
+
+	private String url = "jdbc:sqlserver://localhost:1433" + "Tenta";
+	private String user = "test";
+	private String pw = "test";
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+}
