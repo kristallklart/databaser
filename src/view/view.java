1fa9cb8f37afba68439d8703ed553d6e91482667
fnyberg <fanny.nyberg@live.com>
2016-09-27 09:42:56 +0200

    små fix

--- src/view/view.java
+++ src/view/view.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
