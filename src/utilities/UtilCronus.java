82687d1627e3769c97b8c28fee372f352e6a7c97
Jakob Lindh <hammerslay@gmail.com>
2016-10-02 15:15:08 +0200

    Optimerade filöppningen och rättade stavfel i comboboxarna.

--- src/utilities/UtilCronus.java
+++ src/utilities/UtilCronus.java
@@ -19,7 +19,6 @@ public class UtilCronus {
 	private QueriesCronus queriesCronus = new QueriesCronus();
 
 	public UtilCronus() {
-		cronusAccessDocFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusAccessDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query1.accdb");
 		cronusAccessDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query2.accdb");
 		cronusAccessDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query3.accdb");
@@ -28,7 +27,6 @@ public class UtilCronus {
 		cronusAccessDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query6.accdb");
 		cronusAccessDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query7.accdb");
 
-		cronusExcelDocFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusExcelDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query1.xlsx");
 		cronusExcelDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query2.xlsx");
 		cronusExcelDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query3.xlsx");
@@ -37,7 +35,6 @@ public class UtilCronus {
 		cronusExcelDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query6.xlsx");
 		cronusExcelDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query7.xlsx");
 
-		cronusManagementStudioDocFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusManagementStudioDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query1.sql");
 		cronusManagementStudioDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query2.sql");
 		cronusManagementStudioDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query3.sql");
@@ -46,15 +43,12 @@ public class UtilCronus {
 		cronusManagementStudioDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query6.sql");
 		cronusManagementStudioDocFilesToOpen.add("C:\\Program Files\\Cronusfiler\\Query7.sql");
 
-		cronusAccessFormFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusAccessFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportEmployee.accdb");
 		cronusAccessFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportCustomer.accdb");
 
-		cronusExcelFormFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusExcelFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportEmployee.xlsx");
 		cronusExcelFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportCustomer.xlsx");
 
-		cronusWordFormFilesToOpen.add("NOT IN USE BECAUSE OF INDEXING CALCULATIONS IN THE VIEW CLASS");
 		cronusWordFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportEmployee.docx");
 		cronusWordFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportCustomer.docx");
 	}
@@ -73,7 +67,7 @@ public class UtilCronus {
 
 		cronusQueryNames.add("Select..."); // 0
 		cronusQueryNames.add("Employee"); // 1
-		cronusQueryNames.add("Employee Absence top 20"); // 2
+		cronusQueryNames.add("Employee Absence (Top 20)"); // 2
 		cronusQueryNames.add("Employee Portal Setup"); // 3
 		cronusQueryNames.add("Employee Qualification"); // 4
 		cronusQueryNames.add("Employee Relative"); // 5
@@ -85,19 +79,19 @@ public class UtilCronus {
 	public Vector<String> getCronusQueryGetMetaData() {
 		Vector<String> cronusQueryNames = new Vector<String>();
 		cronusQueryNames.add("Select..."); // 0
-		cronusQueryNames.add("Metadata Employee top 20"); // 1
+		cronusQueryNames.add("Metadata Employee (Top 20)"); // 1
 		cronusQueryNames.add("Metadata Employee Absence"); // 2
-		cronusQueryNames.add("Metadata Employee Portal Setup top 20"); // 3
+		cronusQueryNames.add("Metadata Employee Portal Setup (Top 20)"); // 3
 		cronusQueryNames.add("Metadata Employee Qualification"); // 4
 		cronusQueryNames.add("Metadata Employee Relative"); // 5
 		cronusQueryNames.add("Metadata Employee Statistics Group"); // 6
-		cronusQueryNames.add("Keys top 20"); // 7
-		cronusQueryNames.add("Indexes top 20"); // 8
-		cronusQueryNames.add("Table Constrains top 20"); // 9
-		cronusQueryNames.add("All tables (InformationSchema)"); // 10
-		cronusQueryNames.add("All tables (SysObjects)"); // 11
-		cronusQueryNames.add("Employee columns (InformationSchema)"); // 12
-		cronusQueryNames.add("Employee columns (SysColumns)"); // 13
+		cronusQueryNames.add("Keys (Top 20)"); // 7
+		cronusQueryNames.add("Indexes (Top 20)"); // 8
+		cronusQueryNames.add("Table Constraints (Top 20)"); // 9
+		cronusQueryNames.add("All tables (Using Information Schema)"); // 10
+		cronusQueryNames.add("All tables (Using SysObjects)"); // 11
+		cronusQueryNames.add("Employee columns (Using Informatio nSchema)"); // 12
+		cronusQueryNames.add("Employee columns (Using SysColumns)"); // 13
 		cronusQueryNames.add("Most rows in database"); // 14
 
 		return cronusQueryNames;
@@ -110,8 +104,8 @@ public class UtilCronus {
 		cronusQueryFileNames.add("100 NOK in SEK"); // 1
 		cronusQueryFileNames.add("Most used currency in SEK"); // 2
 		cronusQueryFileNames.add("Adress and town for Fotograferna AB"); // 3
-		cronusQueryFileNames.add("Name for employees who has been sick"); // 4
-		cronusQueryFileNames.add("Name and kinship for all employees' relatives"); // 5
+		cronusQueryFileNames.add("Name of employees who has been sick"); // 4
+		cronusQueryFileNames.add("Name and kinship of all employees' relatives"); // 5
 		cronusQueryFileNames.add("Customers handled by Andreas Berglund"); // 6
 		cronusQueryFileNames.add("Bank accounts owned by customer nr 10000"); // 7
 
@@ -130,17 +124,17 @@ public class UtilCronus {
 		// samt indexet för vilken cronusfil som ska öppnas så öppnas rätt
 		// fil med hjälp av desktop-klassen /JAKOB
 		if (nameProgramToUse.equals("comboBox_access_excel") && selectedProgramToUse == 1) {
-			desktop.open(new File(cronusAccessDocFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusAccessDocFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_access_excel") && selectedProgramToUse == 2) {
-			desktop.open(new File(cronusExcelDocFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusExcelDocFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_access_excel") && selectedProgramToUse == 3) {
-			desktop.open(new File(cronusManagementStudioDocFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusManagementStudioDocFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_oform_selectProgram") && selectedProgramToUse == 1) {
-			desktop.open(new File(cronusAccessFormFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusAccessFormFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_oform_selectProgram") && selectedProgramToUse == 2) {
-			desktop.open(new File(cronusExcelFormFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusExcelFormFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_oform_selectProgram") && selectedProgramToUse == 3) {
-			desktop.open(new File(cronusWordFormFilesToOpen.get(selectedCronusFileToOpen)));
+			desktop.open(new File(cronusWordFormFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		}
 	}
 }
