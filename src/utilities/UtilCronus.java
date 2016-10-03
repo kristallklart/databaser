94c86d7ea4bc028483d28abe195419973a0c0119
paulssonkalle <paulsson.kalle@gmail.com>
2016-10-03 11:58:45 +0200

    små fix

--- src/utilities/UtilCronus.java
+++ src/utilities/UtilCronus.java
@@ -57,38 +57,38 @@ public class UtilCronus {
 		cronusWordFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportEmployee.docx");
 		cronusWordFormFilesToOpen.add("C:\\Program Files\\Cronusfiler\\RapportCustomer.docx");
 
-		cronusQueryFileNames.add("Select..."); // 0
-		cronusQueryFileNames.add("100 NOK in SEK"); // 1
-		cronusQueryFileNames.add("Most used currency in SEK"); // 2
-		cronusQueryFileNames.add("Adress and town for Fotograferna AB"); // 3
-		cronusQueryFileNames.add("Name of employees who has been sick"); // 4
-		cronusQueryFileNames.add("Name and kinship of all employees' relatives"); // 5
-		cronusQueryFileNames.add("Customers handled by Andreas Berglund"); // 6
-		cronusQueryFileNames.add("Bank accounts owned by customer nr 10000"); // 7
+		cronusQueryFileNames.add("Select...");
+		cronusQueryFileNames.add("100 NOK in SEK");
+		cronusQueryFileNames.add("Most used currency in SEK");
+		cronusQueryFileNames.add("Adress and town for Fotograferna AB");
+		cronusQueryFileNames.add("Name of employees who has been sick");
+		cronusQueryFileNames.add("Name and kinship of all employees' relatives");
+		cronusQueryFileNames.add("Customers handled by Andreas Berglund");
+		cronusQueryFileNames.add("Bank accounts owned by customer nr 10000");
 
-		cronusQueryMetaDataNames.add("Select..."); // 0
-		cronusQueryMetaDataNames.add("Metadata Employee (Top 20)"); // 1
-		cronusQueryMetaDataNames.add("Metadata Employee Absence"); // 2
-		cronusQueryMetaDataNames.add("Metadata Employee Portal Setup (Top 20)"); // 3
-		cronusQueryMetaDataNames.add("Metadata Employee Qualification"); // 4
-		cronusQueryMetaDataNames.add("Metadata Employee Relative"); // 5
-		cronusQueryMetaDataNames.add("Metadata Employee Statistics Group"); // 6
-		cronusQueryMetaDataNames.add("Keys (Top 20)"); // 7
-		cronusQueryMetaDataNames.add("Indexes (Top 20)"); // 8
-		cronusQueryMetaDataNames.add("Table Constraints (Top 20)"); // 9
-		cronusQueryMetaDataNames.add("All tables (Using Information Schema)"); // 10
-		cronusQueryMetaDataNames.add("All tables (Using SysObjects)"); // 11
-		cronusQueryMetaDataNames.add("Employee columns (Using Information Schema)"); // 12
-		cronusQueryMetaDataNames.add("Employee columns (Using SysColumns)"); // 13
-		cronusQueryMetaDataNames.add("Most rows in database"); // 14
+		cronusQueryMetaDataNames.add("Select...");
+		cronusQueryMetaDataNames.add("Metadata Employee (Top 20)");
+		cronusQueryMetaDataNames.add("Metadata Employee Absence");
+		cronusQueryMetaDataNames.add("Metadata Employee Portal Setup (Top 20)");
+		cronusQueryMetaDataNames.add("Metadata Employee Qualification");
+		cronusQueryMetaDataNames.add("Metadata Employee Relative");
+		cronusQueryMetaDataNames.add("Metadata Employee Statistics Group");
+		cronusQueryMetaDataNames.add("Keys (Top 20)");
+		cronusQueryMetaDataNames.add("Indexes (Top 20)");
+		cronusQueryMetaDataNames.add("Table Constraints (Top 20)");
+		cronusQueryMetaDataNames.add("All tables (Using Information Schema)");
+		cronusQueryMetaDataNames.add("All tables (Using SysObjects)");
+		cronusQueryMetaDataNames.add("Employee columns (Using Information Schema)");
+		cronusQueryMetaDataNames.add("Employee columns (Using SysColumns)");
+		cronusQueryMetaDataNames.add("Most rows in database");
 
-		cronusQueryTableNames.add("Select..."); // 0
-		cronusQueryTableNames.add("Employee"); // 1
-		cronusQueryTableNames.add("Employee Absence (Top 20)"); // 2
-		cronusQueryTableNames.add("Employee Portal Setup"); // 3
-		cronusQueryTableNames.add("Employee Qualification"); // 4
-		cronusQueryTableNames.add("Employee Relative"); // 5
-		cronusQueryTableNames.add("Employee Statistics Group"); // 6
+		cronusQueryTableNames.add("Select...");
+		cronusQueryTableNames.add("Employee");
+		cronusQueryTableNames.add("Employee Absence (Top 20)");
+		cronusQueryTableNames.add("Employee Portal Setup");
+		cronusQueryTableNames.add("Employee Qualification");
+		cronusQueryTableNames.add("Employee Relative");
+		cronusQueryTableNames.add("Employee Statistics Group");
 	}
 
 	public String getQuery(String comboBoxName, int selectedIndex) {
@@ -114,15 +114,9 @@ public class UtilCronus {
 
 	public void openCronusFile(String nameProgramToUse, int selectedProgramToUse, int selectedCronusFileToOpen)
 			throws IOException, IllegalArgumentException {
-		// Desktop är en klass som används för att bland annat göra så att
-		// en Java-applikation kan öppna filer.
-		// Därför behöver vi en sådan här. /JAKOB
+
 		Desktop desktop = Desktop.getDesktop();
 
-		// Efter att ha tagit emot namnet på vilken combobox som används,
-		// indexet för comboboxen som väljer program,
-		// samt indexet för vilken cronusfil som ska öppnas så öppnas rätt
-		// fil med hjälp av desktop-klassen /JAKOB
 		if (nameProgramToUse.equals("comboBox_access_excel") && selectedProgramToUse == 1) {
 			desktop.open(new File(cronusAccessDocFilesToOpen.get(selectedCronusFileToOpen - 1)));
 		} else if (nameProgramToUse.equals("comboBox_access_excel") && selectedProgramToUse == 2) {
